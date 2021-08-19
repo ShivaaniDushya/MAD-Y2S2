5b1c3ef1aca0407a379f552a0aeca08b2c820a29
Thilan Gavinda <71542691+it20067724@users.noreply.github.com>
2021-08-19 13:47:25 +0530

    Update Routes.java
    Update navigation

--- app/src/main/java/com/example/mobileapplication/Routes.java
+++ app/src/main/java/com/example/mobileapplication/Routes.java
@@ -5,21 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import com.google.android.filament.View;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Routes extends AppCompatActivity {
-    private ImageButton button;
+    private ImageButton imageButton;
+    private Button button;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
-
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -28,32 +30,35 @@ public class Routes extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.items:
                         startActivity(new Intent(getApplicationContext()
                                 , Items.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.customers:
                         startActivity(new Intent(getApplicationContext()
-                                , ModifyRoute.class));
-                        overridePendingTransition(0,0);
+                                , Customers.class));
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.home:
                         startActivity(new Intent(getApplicationContext()
                                 , MainActivity.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.routes:
+                        startActivity(new Intent(getApplicationContext()
+                                , Routes.class));
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.sales:
                         startActivity(new Intent(getApplicationContext()
-                                , AddRoute.class));
-                        overridePendingTransition(0,0);
+                                , Sales.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
 
@@ -61,7 +66,50 @@ public class Routes extends AppCompatActivity {
             }
         });
 
+        imageButton= findViewById(R.id.btn_view_route);
 
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openViewRoute();
+            }
+        });
+        //start code for Add new route button
+        //pls replace below code after float button in UI
+
+        button = (Button) findViewById(R.id.btn_add_new_route);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddNewRoute();
+            }
+        });
+
+    }   //close the tag pls place over this any code related to on start
+
+    //function to load the add route page.
+
+    public void openAddNewRoute() {
+        Intent intent = new Intent(this, AddRoute.class);
+        startActivity(intent);
+        Log.i("Lifecycle", "Add route button clicked");
     }
 
+    //finish code for Add new Route
+
+
+    //start code for view route button
+
+    public void openViewRoute()
+    {
+        Intent intent = new Intent(this,ModifyRoute.class);
+        startActivity(intent);
+        Log.i("Lifecycle","Add route button clicked");
     }
+
+    //finish code for Add new Route
+}
+
+
+
