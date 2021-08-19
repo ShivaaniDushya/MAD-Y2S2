cfc1e8cd6cb8d2f057106fec31b9e7565bffe723
Thilan Gavinda <71542691+it20067724@users.noreply.github.com>
2021-08-19 22:20:23 +0530

    Crud operation add function added
    Implemented button functionality

--- app/src/main/java/com/example/mobileapplication/AddRoute.java
+++ app/src/main/java/com/example/mobileapplication/AddRoute.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.mobileapplication.database.DBHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AddRoute extends AppCompatActivity {
-
+    EditText etstartloc,etendloc,etdistance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_route);
 
+        etstartloc=findViewById(R.id.inp_startloc);
+        etendloc=findViewById(R.id.inp_endloc);
+        etdistance=findViewById(R.id.inp_distance);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.routes);
@@ -57,4 +65,21 @@ public class AddRoute extends AppCompatActivity {
         });
 
     }
+
+    public void addRoute(View view){
+        DBHelper dbHelper=new DBHelper(this);
+
+        long val=dbHelper.addRoutes(etstartloc.getText().toString(),
+                etendloc.getText().toString(),
+                Float.parseFloat(etdistance.getText().toString()));
+
+        Toast.makeText(this, "Record Added Succesfully", Toast.LENGTH_SHORT).show();
+
+        Intent intent=new Intent(this, Routes.class);
+        startActivity(intent);
+        Log.i("BTN Click", "Add route Confirmation button clicked");
+    }
+
+
+
 }
