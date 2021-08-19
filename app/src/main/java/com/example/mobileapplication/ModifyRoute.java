76c6af005fc548ab41300a87125dc57f16cc5d3a
Thilan Gavinda <71542691+it20067724@users.noreply.github.com>
2021-08-19 22:22:20 +0530

    Update and Delete functinality
    Added update and Delete fuctionality

--- app/src/main/java/com/example/mobileapplication/ModifyRoute.java
+++ app/src/main/java/com/example/mobileapplication/ModifyRoute.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.mobileapplication.database.DBHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ModifyRoute extends AppCompatActivity {
-
+    EditText etrouteid,etstartloc,etendloc,etdistance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_route);
+
+        etrouteid=findViewById(R.id.inp_rid);
+        etstartloc=findViewById(R.id.inp_startlocup);
+        etendloc=findViewById(R.id.inp_endlocup);
+        etdistance=findViewById(R.id.inp_distanceup);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.routes);
@@ -55,5 +65,30 @@ public class ModifyRoute extends AppCompatActivity {
             }
         });
 
+    }
+    public void updateRoute(View view){
+        DBHelper dbHelper=new DBHelper(this);
+
+        int val=dbHelper.updateRoute(etrouteid.getText().toString(),etstartloc.getText().toString(),
+                etendloc.getText().toString(),
+                Float.parseFloat(etdistance.getText().toString()));
+
+        Toast.makeText(this, "Record Updated Succesfully", Toast.LENGTH_SHORT).show();
+
+        Intent intent=new Intent(this, Routes.class);
+        startActivity(intent);
+        Log.i("BTN Click", "Update route Confirmation button clicked");
+    }
+
+    public void deleteRoute(View view){
+        DBHelper dbHelper=new DBHelper(this);
+
+         dbHelper.deleteRoute(etrouteid.getText().toString());
+
+        Toast.makeText(this, "Record Deleted Succesfully", Toast.LENGTH_SHORT).show();
+
+        Intent intent=new Intent(this, Routes.class);
+        startActivity(intent);
+        Log.i("BTN Click", "Delete route Confirmation button clicked");
     }
 }
