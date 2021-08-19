1938229dbabc280b26f3001b08f7fcdfa1063dc3
ShivaaniDushya <it20006884@my.sliit.lk>
2021-08-19 21:18:49 +0530

    Second-commit

---
+++ app/src/main/java/com/example/mobileapplication/Edit_item_Activity.java
@@ -0,0 +1,15 @@
+package com.example.mobileapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_item_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+
+    }
+}
--- app/src/main/java/com/example/mobileapplication/Items.java
+++ app/src/main/java/com/example/mobileapplication/Items.java
@@ -1,13 +1,16 @@
 package com.example.mobileapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Items extends AppCompatActivity {
 
@@ -15,6 +18,10 @@ public class Items extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        View view_item_btn;
+        FloatingActionButton create_item_btn;
+        ImageButton imgbtn;
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.items);
@@ -53,6 +60,53 @@ public class Items extends AppCompatActivity {
 
                 return false;
             }
+
+        });
+
+
+        //create button
+        create_item_btn = (FloatingActionButton) findViewById(R.id.create_item_button);
+        create_item_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityCreateItem();
+            }
+        });
+
+        //"View an Item" button
+        view_item_btn = (View) findViewById(R.id.view_action1);
+        view_item_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityViewItem();
+            }
+
+        });
+
+        //edit pencil icon
+        imgbtn = (ImageButton) findViewById(R.id.imageButton);
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityEditItem();
+            }
         });
     }
+
+    public void openActivityCreateItem() {
+        Intent intent = new Intent(this, Create_item_Activity.class);
+        startActivity(intent);
+    }
+
+    private void openActivityViewItem() {
+        Intent intent = new Intent(this, View_item_Activity.class);
+        startActivity(intent);
+    }
+
+    public void openActivityEditItem() {
+        Intent intent = new Intent(this, Edit_item_Activity.class);
+        startActivity(intent);
+    }
+
+
 }
