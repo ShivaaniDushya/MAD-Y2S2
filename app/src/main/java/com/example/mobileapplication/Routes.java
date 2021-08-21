4dd103c75ed279333979f81051ebfce754c574ed
Thilan Gavinda <71542691+it20067724@users.noreply.github.com>
2021-08-22 01:10:38 +0530

    Updating Crud Operations

--- app/src/main/java/com/example/mobileapplication/Routes.java
+++ app/src/main/java/com/example/mobileapplication/Routes.java
@@ -1,27 +1,80 @@
 package com.example.mobileapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.mobileapplication.database.DBHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Routes extends AppCompatActivity {
-    private ImageButton imageButton;
-    private Button button;
+
+    RecyclerView recyclerView;
+    FloatingActionButton fab;
+    ImageView empty_imageview;
+    TextView no_data;
+
+    DBHelper db;
+    ArrayList<String> route_id,route_start_location,route_end_location,route_distance,route_created_date,route_modified_date,route_isdefault;
+    RouteAdapter routeAdapter;
+
+
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
+
+        Log.d("workflow","Routes on_create method Called");
+
+        recyclerView=findViewById(R.id.recyclerView);
+        empty_imageview=findViewById(R.id.empty_image);
+        no_data = findViewById(R.id.no_data);
+
+        db =new DBHelper(this);
+        route_id = new ArrayList<>();
+
+        route_start_location = new ArrayList<>();
+        route_end_location = new ArrayList<>();
+        route_distance = new ArrayList<>();
+        route_created_date = new ArrayList<>();
+        route_modified_date = new ArrayList<>();
+        route_isdefault = new ArrayList<>();
+
+        storeDataInArrays();
+        Log.d("workflow","Routes storeDataInArrays method Called");
+
+        routeAdapter = new RouteAdapter(this,this,route_id,
+                route_start_location,
+                route_end_location,
+                route_distance,
+                route_created_date,
+                route_modified_date,
+                route_isdefault);
+
+        recyclerView.setAdapter(routeAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -61,36 +114,69 @@ public class Routes extends AppCompatActivity {
                         overridePendingTransition(0, 0);
                         return true;
                 }
-
+                Log.d("workflow","Routes bottom navigation method Called");
                 return false;
             }
         });
 
-        imageButton= findViewById(R.id.btn_view_route);
 
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openViewRoute();
-            }
-        });
         //start code for Add new route button
         //pls replace below code after float button in UI
 
-        button = (Button) findViewById(R.id.btn_add_new_route);
+        fab = findViewById(R.id.btn_add_route);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 openAddNewRoute();
+                Log.d("workflow","Routes Float Button Clicked");
             }
         });
 
+
+
+
     }   //close the tag pls place over this any code related to on start
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==1){
+            recreate();    //refresh if data not fetched
+            Log.d("workflow","Routes onActivityResult method Called");
+        }
+    }
+
+    private void storeDataInArrays() {
+        Log.d("workflow","Routes storeDataInArrays method Called");
+        Cursor cursor=db.readAllRoutes();
+        if(cursor.getCount()==0){
+            empty_imageview.setVisibility(View.VISIBLE);
+            no_data.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            while(cursor.moveToNext()){
+                route_id.add(cursor.getString(0));
+                route_start_location.add(cursor.getString(1));
+                route_end_location.add(cursor.getString(2));
+                route_distance.add(cursor.getString(3));
+                route_created_date.add(cursor.getString(4));
+                route_modified_date.add(cursor.getString(5));
+                route_isdefault.add(cursor.getString(6));
+            }
+            empty_imageview.setVisibility(View.GONE);
+            no_data.setVisibility(View.GONE);
+        }
+    }
+
 
     //function to load the add route page.
 
     public void openAddNewRoute() {
+        Log.d("workflow","Routes openAddNewRoute method Called");
         Intent intent = new Intent(this, AddRoute.class);
         startActivity(intent);
         Log.i("Lifecycle", "Add route button clicked");
@@ -103,12 +189,17 @@ public class Routes extends AppCompatActivity {
 
     public void openViewRoute()
     {
+        Log.d("workflow","Routes openViewRoute method Called");
         Intent intent = new Intent(this,ModifyRoute.class);
         startActivity(intent);
         Log.i("Lifecycle","Add route button clicked");
     }
 
     //finish code for Add new Route
+
+
+
+
 }
 
 
