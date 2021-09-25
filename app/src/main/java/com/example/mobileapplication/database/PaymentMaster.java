e438a4be21e4d8663e9299480b470f58998dc2d2
Thilan Gavinda <71542691+it20067724@users.noreply.github.com>
2021-09-25 13:26:51 +0530

    Revert "sales and update-payment"

--- app/src/main/java/com/example/mobileapplication/database/PaymentMaster.java
+++ app/src/main/java/com/example/mobileapplication/database/PaymentMaster.java
@@ -7,12 +7,13 @@ public class PaymentMaster {
     public PaymentMaster() {
     }
 
-    public static class PaymentT implements BaseColumns {
+    public static class PaymentsT implements BaseColumns {
         public static final String TABLE_NAME = "payments";
         public static final String COLUMN_NAME_INVOICE_ID = "inv_id";
+        public static final String COLUMN_NAME_INVOICE_AMOUNT = "inv_amount";
         public static final String COLUMN_NAME_PAYMENT = "payment";
         public static final String COLUMN_NAME_PAYMENT_DATE = "date";
-        //public static final String COLUMN_NAME_BALANCE = "balance";
+        public static final String COLUMN_NAME_BALANCE = "balance";
     }
 
 }
